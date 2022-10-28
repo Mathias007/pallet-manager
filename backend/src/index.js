ac59afbe8aec78dbd175c8f07e879e320b27f01e
Mathias007 <mathias.najlepszy@gmail.com>
2022-10-28 23:27:02 +0200

    Build API routes using notifications' controllers.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,23 @@
 import cors from "cors";
 import express from "express";
+import mongoose from "mongoose";
 
 import routes from "./routes";
 
-import { PORT_LISTENING_START_MESSAGE } from "./config/messages";
-import { paths } from "./config/names";
+import {
+    PORT_LISTENING_START_MESSAGE,
+    DATABASE_CONNECTION_SUCCESS_MESSAGE,
+    DATABASE_CONNECTION_ERROR_MESSAGE,
+} from "./config/messages";
+
 import config from "./config/config";
 
-const { root } = paths;
-const { NODE_PORT, CORS_ORIGIN } = config;
+import { initializeNotificationsData } from "./seed/notification.seeder";
+
+import routesPaths from "./config/routesPaths";
+const { ROOT } = routesPaths;
+
+const { NODE_PORT, MONGO_URI, CORS_ORIGIN } = config;
 
 const app = express();
 
@@ -17,8 +26,31 @@ const corsOptions = {
     origin: CORS_ORIGIN || "",
 };
 app.use(cors(corsOptions));
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
 
-app.use(root, routes);
+mongoose
+    .connect(MONGO_URI, {
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+    .then(console.log(DATABASE_CONNECTION_SUCCESS_MESSAGE()))
+    .catch((err) => console.log(DATABASE_CONNECTION_ERROR_MESSAGE(err)));
+
+mongoose.connection.on("connected", () => {
+    initializeNotificationsData();
+    console.log("Initialize notification");
+});
+mongoose.connection.on("error", (err) => {
+    console.log(DATABASE_CONNECTION_ERROR_MESSAGE(err));
+});
+
+app.use(ROOT, routes);
 
 app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
     console.log(PORT_LISTENING_START_MESSAGE(NODE_PORT || 5000));
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,27 @@
 import express from "express";
 
+import {
+    getNotificationsList,
+    getNotificationById,
+    createNotification,
+    createManyNotifications,
+    modifNotificationById,
+    deleteNotificationById,
+} from "./controllers/notifications.controller";
+
 import { ROOT_API_MESSAGE } from "./config/messages";
 
-import { paths } from "./config/names";
-const { root } = paths;
+import routesPaths from "./config/routesPaths";
+const { ROOT, NOTIFICATIONS } = routesPaths;
 
 const router = express.Router();
 
-router.get(root, ROOT_API_MESSAGE);
+router.get(ROOT, ROOT_API_MESSAGE);
+router.get(NOTIFICATIONS.LIST, getNotificationsList);
+router.post(NOTIFICATIONS.SINGLE, getNotificationById);
+router.post(NOTIFICATIONS.ADD, createNotification);
+router.post(NOTIFICATIONS.ADD_MANY, createManyNotifications);
+router.patch(NOTIFICATIONS.EDIT, modifNotificationById);
+router.delete(NOTIFICATIONS.REMOVE, deleteNotificationById);
 
 export default router;
