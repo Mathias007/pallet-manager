e3114dd1fa76e23fde8b3c13ea8d6888e290999a
Mathias007 <mathias.najlepszy@gmail.com>
2022-10-29 23:37:44 +0200

    Implement Antd and router in App.js and index.js

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,26 +1,26 @@
 import React, { useEffect } from "react";
-import { useForm } from "react-hook-form";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from "react-router-dom";
+
+import AppMain from "../components/sections/AppMain";
+import Home from "../components/pages/Home";
+import Tables from "../components/pages/Tables";
+
+import "antd/dist/antd.min.css";
+import "./styles/main.css";
+import "./styles/responsive.css";
 
 import { useSelector, useDispatch } from "react-redux";
-
 import { notifications } from "../_store/_actions";
 
-import "./App.css";
+import routesPaths from "../_config/routesPaths";
+const { GENERAL } = routesPaths;
 
 function App() {
-    const { register, handleSubmit } = useForm();
-
-    const onSubmit = async (data) => {
-        const formData = new FormData();
-        formData.append("file", data.file[0]);
-
-        const res = await fetch("http://localhost:5000/notifications/upload", {
-            method: "POST",
-            body: formData,
-        }).then((res) => res.json());
-        console.log(JSON.stringify(`${res.message}, status: ${res.status}`));
-    };
-
     const notificationsList = useSelector(
         (state) => state.notifications.notificationsList
     );
@@ -29,23 +29,28 @@ function App() {
 
     useEffect(() => {
         dispatch(notifications.showNotificationsList());
-    }, [dispatch]);
+    }, []);
 
     return (
         <div className="App">
-            <header className="App-header">
-                <button
-                    aria-label="Increment value"
-                    onClick={() => console.log(notificationsList)}
-                >
-                    START
-                </button>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <input type="file" {...register("file")} />
-
-                    <input type="submit" />
-                </form>
-            </header>
+            <Router>
+                <AppMain>
+                    <Switch>
+                        <Route
+                            exact
+                            path={GENERAL.DASHBOARD}
+                            component={Home}
+                        />
+                        <Route
+                            exact
+                            path={GENERAL.TABLES}
+                            component={Tables}
+                            notificationsList={notificationsList}
+                        />
+                        <Redirect from="*" to={GENERAL.DASHBOARD} />
+                    </Switch>
+                </AppMain>
+            </Router>
         </div>
     );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 
+import { Provider } from "react-redux";
 import { store } from "./_store/store";
 
-import "./index.css";
 import App from "./App/App";
 
 import reportWebVitals from "./_config/reportWebVitals";
@@ -18,7 +17,4 @@ root.render(
     </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
